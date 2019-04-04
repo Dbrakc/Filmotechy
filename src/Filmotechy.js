9180976b1457ddfea64dcccb34dd04ed0b530d2e
David Braga <dbraga1985@gmail.com>
2019-04-04 23:39:58 +0200

    Filmotechy base

---
+++ src/Filmotechy.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import 'typeface-roboto';
+
+import './Filmotechy.css'
+
+
+ const Filmotechy = () =>
+    <BrowserRouter>
+    </BrowserRouter>
+
+export default Filmotechy
+
